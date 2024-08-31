34e38d3196d55db00568d3c3cedfc261c6c9dc89
zelalemzola <tzelalemtesfaye@gmail.com>
2024-08-31 14:21:38 +0300

    navbar done

--- src/app/workers/page.jsx
+++ src/app/workers/page.jsx
@@ -122,9 +122,9 @@ const Workers = () => {
   const router = useRouter();
   return (
     <div className="w-full mx-auto mt-[20px] flex flex-col ">
-      <div className="w-full flex flex-wrap items-center justify-between fixed top-0 z-30 bg-white border-b rounded-b-2xl px-[5%] md:px-[10%] py-3 shadow-md">
+      <div className="w-full flex flex-wrap items-center justify-between fixed top-0 z-30 bg-white border-b rounded-b-2xl px-[2%] md:px-[10%] py-3 shadow-md">
       <Link href='/'><ArrowLeft fontWeight='bold' size={33} className="text-white bg-secondary font-bold p-1 rounded-full md:scale-200 cursor-pointer" /></Link>
-        <p className="text-secondary text-lg md:text-2xl font-bold flex items-center gap-6">Available Accountants</p>
+        <p className="text-secondary text-md md:text-2xl font-bold flex items-center gap-6">Available Accountants</p>
         <Dialog className="flex-end">
           <DialogTrigger>
             <Button className="flex items-center gap-3 bg-secondary hover:bg-secondary">Filter <FilterIcon /></Button>
